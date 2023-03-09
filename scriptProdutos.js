b329d1767d60a9ff77ba7d35a0e8b045a56e06b2
Juliana Lima <111203538+Juliana-L1ma@users.noreply.github.com>
2023-03-09 11:17:59 -0300

    Add files via upload

---
+++ scriptProdutos.js
@@ -0,0 +1,90 @@
+
+
+fetch('produtos.json').then(response => response.json())
+.then(informacoes => {
+
+     /*---------------------------Produto 1 -----------------------------------*/
+    document.getElementById("nomeProduto1").innerHTML = informacoes.sabonete.nome;
+    document.getElementById("id").innerHTML = "ID do produto " + informacoes.sabonete.id;
+    document.getElementById("valorDolarProduto1").innerHTML =informacoes.sabonete.valor;
+    var valorDolarProduto1 = informacoes.sabonete.valor; 
+    document.getElementById("valorDolarProduto1").innerHTML = "U$ " + valorDolarProduto1;
+
+
+     /*---------------------------Produto 2 -----------------------------------*/
+     document.getElementById("nomeProduto2").innerHTML = informacoes.shampoo.nome;
+     document.getElementById("id2").innerHTML = "ID do produto " + informacoes.shampoo.id;
+     document.getElementById("valorDolarProduto2").innerHTML =informacoes.shampoo.valor;
+     var valorDolarProduto2 = informacoes.shampoo.valor; 
+     document.getElementById("valorDolarProduto2").innerHTML = "U$ " + valorDolarProduto2;
+
+      /*---------------------------Produto 3 -----------------------------------*/
+    document.getElementById("nomeProduto3").innerHTML = informacoes.condicionador.nome;
+    document.getElementById("id3").innerHTML = "ID do produto " + informacoes.condicionador.id;
+    document.getElementById("valorDolarProduto3").innerHTML =informacoes.condicionador.valor;
+    var valorDolarProduto3 = informacoes.condicionador.valor; 
+    document.getElementById("valorDolarProduto3").innerHTML = "U$ " + valorDolarProduto3;
+
+     /*---------------------------Produto 4 -----------------------------------*/
+     document.getElementById("nomeProduto4").innerHTML = informacoes.kit1.nome;
+     document.getElementById("id4").innerHTML = "ID do produto " + informacoes.kit1.id;
+     document.getElementById("valorDolarProduto4").innerHTML =informacoes.kit1.valor;
+     var valorDolarProduto4 = informacoes.kit1.valor; 
+     document.getElementById("valorDolarProduto4").innerHTML = "U$ " + valorDolarProduto1;
+
+      /*---------------------------Produto 5 -----------------------------------*/
+    document.getElementById("nomeProduto5").innerHTML = informacoes.argila.nome;
+    document.getElementById("id5").innerHTML = "ID do produto " + informacoes.argila.id;
+    document.getElementById("valorDolarProduto5").innerHTML =informacoes.argila.valor;
+    var valorDolarProduto5 = informacoes.argila.valor; 
+    document.getElementById("valorDolarProduto5").innerHTML = "U$ " + valorDolarProduto1;
+
+     /*---------------------------Produto 6 -----------------------------------*/
+     document.getElementById("nomeProduto6").innerHTML = informacoes.oleo.nome;
+     document.getElementById("id6").innerHTML = "ID do produto " + informacoes.oleo.id;
+     document.getElementById("valorDolarProduto6").innerHTML =informacoes.oleo.valor;
+     var valorDolarProduto6 = informacoes.oleo.valor; 
+     document.getElementById("valorDolarProduto6").innerHTML = "U$ " + valorDolarProduto1;
+
+    fetch (`https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL`).then(response => {
+        return response.json() })
+        .then(economia => {
+    
+        var dolarAtual = economia.EURBRL.bid;
+
+        /*---------------------------Produto 1 -----------------------------------*/
+        var convertidoValorProduto1 = valorDolarProduto1 * dolarAtual;
+        var converValorProduto1 = convertidoValorProduto1.toFixed(2);
+        document.getElementById("valorRealProduto1").innerHTML = "R$" + converValorProduto1;
+
+         /*---------------------------Produto 2 -----------------------------------*/
+         var convertidoValorProduto2 = valorDolarProduto2 * dolarAtual;
+         var converValorProduto2 = convertidoValorProduto2.toFixed(2);
+         document.getElementById("valorRealProduto2").innerHTML = "R$" + converValorProduto2;
+
+          /*---------------------------Produto 3 -----------------------------------*/
+        var convertidoValorProduto3 = valorDolarProduto3 * dolarAtual;
+        var converValorProduto3 = convertidoValorProduto3.toFixed(2);
+        document.getElementById("valorRealProduto3").innerHTML = "R$" + converValorProduto3;
+
+         /*---------------------------Produto 4 -----------------------------------*/
+         var convertidoValorProduto4 = valorDolarProduto4 * dolarAtual;
+         var converValorProduto4 = convertidoValorProduto4.toFixed(2);
+         document.getElementById("valorRealProduto4").innerHTML = "R$" + converValorProduto4;
+
+          /*---------------------------Produto 5 -----------------------------------*/
+        var convertidoValorProduto5 = valorDolarProduto5 * dolarAtual;
+        var converValorProduto5 = convertidoValorProduto5.toFixed(2);
+        document.getElementById("valorRealProduto5").innerHTML = "R$" + converValorProduto5;
+
+         /*---------------------------Produto 6 -----------------------------------*/
+         var convertidoValorProduto6 = valorDolarProduto6 * dolarAtual;
+         var converValorProduto6 = convertidoValorProduto6.toFixed(2);
+         document.getElementById("valorRealProduto6").innerHTML = "R$" + converValorProduto6;
+
+
+        })
+
+
+
+})
